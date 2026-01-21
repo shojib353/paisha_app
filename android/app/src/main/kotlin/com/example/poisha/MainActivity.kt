b6d505f86a580611d748565944a5d02e4a8488c3
Shojib353 <shojib.shojib10@gmail.com>
2026-01-21 18:10:26 +0600

    done

---
+++ android/app/src/main/kotlin/com/example/poisha/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.poisha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+        ),
+        body: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
